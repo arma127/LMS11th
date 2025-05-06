40bf78b102322b95223ba9193902f6eaef96c654
arma127 <153940630+arma127@users.noreply.github.com>
2025-05-06 23:00:09 +0500

    Update authReducer.js

--- authReducer.js
+++ authReducer.js
@@ -1,18 +1,19 @@
-const initialState = {
-    user: null,
-  };
+const savedUser = JSON.parse(localStorage.getItem("user"));
+
+const initialState = { user: savedUser || null };
   
-  export function authReducer(state = initialState, action) {
+export function authReducer(state = initialState, action) {
     switch (action.type) {
       case 'LOGIN':
+        localStorage.setItem("user", JSON.stringify(action.payload));
         return { ...state, user: action.payload };
       case 'LOGOUT':
+        localStorage.removeItem("user");
         return { ...state, user: null };
       default:
         return state;
     }
   }
   
-  export const login = (user) => ({ type: 'LOGIN', payload: user });
-  export const logout = () => ({ type: 'LOGOUT' });
-  
+export const login = (user) => ({ type: 'LOGIN', payload: user });
+export const logout = () => ({ type: 'LOGOUT' });
